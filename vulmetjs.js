a982f91fe6970b9024dfe90599bb9f2b56d4fa99
devkirb <54935373+devkirb@users.noreply.github.com>
2020-03-10 10:57:04 +0100

    vulmetjs.js
    js

---
+++ vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerHTML = "paginatitel"
+document.getElementById("subkoptext").innerHTML = "subkop text"
+document.getElementById("img1").src = "img/img1.jpg"
+document.getElementById("img2").src = "img/img2.png"
