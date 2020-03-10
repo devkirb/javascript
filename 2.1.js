7a2d29cdad1d321e69b070537730d4ab918b3741
devkirb <54935373+devkirb@users.noreply.github.com>
2020-03-10 10:51:45 +0100

    2.1.js
    dijhof

---
+++ 2.1.js
@@ -0,0 +1,9 @@
+var mijnauto ={
+    merk: "Ford",
+    Type: "Cool",
+    aantalWielen: 3,
+    kleur: "Rood"
+}
+
+console.log(mijnauto.kleur);
+document.getElementById("element").innerHTML = mijnauto.kleur
